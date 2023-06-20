6fb80437638d430d41ecd3678a6e53c6c8c3ad43
Roques Steve <steve.roques@gmail.com>
2023-06-20 17:52:56 +0200

    using constant folding

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ use std::io::Read;
 fn main() {
     let mut source = String::new();
     std::io::stdin().read_to_string(&mut source).unwrap();
-    let ast = aleparser::parse(source);
+    let mut ast = aleparser::parse(source);
+
+    ast = constantfolding::transform(ast);
+    
     let response = pythongen::generate(ast);
     
     println!("{}", response);
